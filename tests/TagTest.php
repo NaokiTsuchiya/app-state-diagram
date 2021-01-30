e67b99850da07612715bae825b608f6aadb8012b
tsuchiya <tsuchiya@bengo4.com>
2021-01-30 18:26:02 +0900

    Update TagTest

--- tests/TagTest.php
+++ tests/TagTest.php
@@ -8,26 +8,34 @@ use PHPUnit\Framework\TestCase;
 
 class TagTest extends TestCase
 {
-    /** @var TaggedAlpsProfile */
-    protected $profile;
+    /** @var AlpsProfile */
+    private $profile;
 
     protected function setUp(): void
     {
-        $andTags = ['a', 'b'];
-        $orTags = [];
-        $this->profile = (
-            new TaggedAlpsProfile(
-                new AlpsProfile(__DIR__ . '/Fake/alps_tag.json'),
-                $orTags,
-                $andTags
-            )
-        );
+        $this->profile = new AlpsProfile(__DIR__ . '/Fake/alps_tag.json');
     }
 
-    public function testFilteredBtTag(): void
+    public function testFilteredByAndTag(): void
     {
-        $profile = $this->profile;
+        $profile = (new TaggedAlpsProfile(
+            $this->profile,
+            [],
+            ['a', 'b']
+        ));
         $this->assertArrayHasKey('s1->s2:t1', $profile->links);
         $this->assertArrayNotHasKey('s2->s3:t2', $profile->links);
+    }
+
+    public function testFilteredByOrTag(): void
+    {
+        $profile = (new TaggedAlpsProfile(
+            $this->profile,
+            ['a'],
+            []
+        ));
+        $this->assertArrayHasKey('s1->s2:t1', $profile->links);
+        $this->assertArrayHasKey('s2->s3:t2', $profile->links);
+        $this->assertArrayHasKey('s3->s4:t3', $profile->links);
     }
 }
