e5bd18c5303b708adbd6499446dff0ebf5c481c3
tsuchiya <tsuchiya@bengo4.com>
2021-01-31 18:32:38 +0900

    Add red test

--- tests/TagTest.php
+++ tests/TagTest.php
@@ -25,6 +25,8 @@ class TagTest extends TestCase
         ));
         $this->assertArrayHasKey('s1->s2:t1', $profile->links);
         $this->assertArrayNotHasKey('s2->s3:t2', $profile->links);
+        $this->assertArrayHasKey('s1->s5:t5', $profile->links);
+        $this->assertArrayNotHasKey('s5->s6:s6', $profile->links);
         $this->assertArrayHasKey('id', $profile->descriptors);
         $this->assertCount(2, $profile->descriptors['s1']->descriptor);
         $this->assertCount(1, $profile->descriptors['s2']->descriptor);
@@ -40,6 +42,8 @@ class TagTest extends TestCase
         $this->assertArrayHasKey('s1->s2:t1', $profile->links);
         $this->assertArrayHasKey('s2->s3:t2', $profile->links);
         $this->assertArrayHasKey('s3->s4:t3', $profile->links);
+        $this->assertArrayHasKey('s1->s5:t5', $profile->links);
+        $this->assertArrayHasKey('s5->s6:s6', $profile->links);
         $this->assertArrayHasKey('id', $profile->descriptors);
         $this->assertCount(2, $profile->descriptors['s1']->descriptor);
         $this->assertCount(2, $profile->descriptors['s2']->descriptor);
